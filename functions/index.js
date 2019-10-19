3c6da5ba3f816afe60af8707f2e83c5f23863cc8
kevind123 <thedude136895@gmail.com>
2019-10-18 23:48:25 -0700

    updated functions to use sendgrid.

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,110 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
-const { google } = require("googleapis");
+const sgMail = require('@sendgrid/mail');
+const sendgridConfig = require('./sendgridConfig')
+const SENDGRID_API_KEY = sendgridConfig.SENDGRID_API_KEY
+// const { google } = require("googleapis");
 
 const cors = require('cors')({origin: true});
-const OAuth2 = google.auth.OAuth2;
+// const OAuth2 = google.auth.OAuth2;
 
 admin.initializeApp();
 
 const APP_NAME = 'Footprint';
 
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-const oauth2Client = new OAuth2(
-     "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com", // ClientID
-     "YyDVkhHbPDcbaf_o4dDEfnBW", // Client Secret
-     "https://developers.google.com/oauthplayground" // Redirect URL
-);
-oauth2Client.setCredentials({
-     refresh_token: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs"
-});
 
-console.log("getting accessToken...")
-const accessToken = await oauth2Client.getAccessToken()
-// const {tokens} = await oauth2Client.getToken(code)
-console.log("accessToken: ", accessToken)
+// const code = "4/sAEgJM7V_9aL7HLG91ughzW0E8rfliur-GsoJBj8HXo9on6QpbU9ZP5G9zDgT9-67IVwqHmyS399kPlgQ6362BQ"
 
-let transporter = nodemailer.createTransport({
-		service: "gmail",
-     auth: {
-          type: "OAuth2",
-          user: "footprintlive@gmail.com", 
-          clientId: "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com",
-          clientSecret: "YyDVkhHbPDcbaf_o4dDEfnBW",
-          refreshToken: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs",
-          accessToken: accessToken
-     }
-});
+// const oauth2Client = new OAuth2(
+//      "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com", // ClientID
+//      "YyDVkhHbPDcbaf_o4dDEfnBW", // Client Secret
+//      "https://developers.google.com/oauthplayground" // Redirect URL
+// );
+// oauth2Client.setCredentials({
+//      refresh_token: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs"
+// });
+// 
+// console.log("getting accessToken...")
+// let accessToken
+// let transporter
+
+// oauth2Client.getAccessToken()
+// .then(token => {
+// 	console.log("accessToken received: ", token)
+// 	accessToken = token
+// 
+// 	transporter = nodemailer.createTransport({
+// 		service: "gmail",
+//      auth: {
+//           type: "OAuth2",
+//           user: "footprintlive@gmail.com", 
+//           clientId: "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com",
+//           clientSecret: "YyDVkhHbPDcbaf_o4dDEfnBW",
+//           refreshToken: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs",
+//           accessToken: accessToken
+//      }
+// 	});
+// 
+// 	console.log("accessToken: ", accessToken, ", transporter: ", transporter)
+// 
+// 	return token
+// })
+// .catch(erro => {
+// 	console.log("Error: ", erro)
+// 	throw erro
+// })
+
+// const {tokens} = oauth2Client.getToken("4/sAEgJM7V_9aL7HLG91ughzW0E8rfliur-GsoJBj8HXo9on6QpbU9ZP5G9zDgT9-67IVwqHmyS399kPlgQ6362BQ")
+// .then(token => {
+// 	console.log("token resp: ", token)
+// 	return token
+// })
+// .catch(erro => {
+// 	console.log("erro: " erro)
+// 	throw erro
+// })
+
+// const getToken = async () => {
+//   const { tokens } = await oauth2Client.getToken(code);
+//   console.info(tokens);
+// 
+//   oauth2Client.setCredentials(tokens);
+// 
+//   accessToken = tokens
+// 	console.log("accessToken: ", accessToken, ", tokens: ", tokens)
+// 
+// 	transporter = nodemailer.createTransport({
+// 		service: "gmail",
+//      auth: {
+//           type: "OAuth2",
+//           user: "footprintlive@gmail.com", 
+//           clientId: "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com",
+//           clientSecret: "YyDVkhHbPDcbaf_o4dDEfnBW",
+//           refreshToken: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs",
+//           accessToken: accessToken
+//      }
+// });
+// };
+//  
+// getToken();
+
+
+
+
+// let transporter = nodemailer.createTransport({
+// 		service: "gmail",
+//      auth: {
+//           type: "OAuth2",
+//           user: "footprintlive@gmail.com", 
+//           clientId: "13408894856-8s85fjfvnpujkk549piu9drovsh937jv.apps.googleusercontent.com",
+//           clientSecret: "YyDVkhHbPDcbaf_o4dDEfnBW",
+//           refreshToken: "1//04p-w7bh6GwpaCgYIARAAGAQSNwF-L9IrsHslmEW3nKRCrSRgjsi30K_D-e-uYxzF6Jc2ZqLuc9kv0KRCtk3VR2fn9BdkF62ELXs",
+//           accessToken: accessToken
+//      }
+// });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -46,43 +115,68 @@ let transporter = nodemailer.createTransport({
 
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 	console.log("sendWelcomEmail started, user.email: ", user.email)
-	const mailOptions = {
-    from: 'Footprint Support <footprintlive@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
-    to: user.email,
-    subject: 'Welcome to Footprint!',
-    html: `<h2 style="font-size: 16px;">Welcome to Footprint!</h2>
-      <p>Now you can start sharing your footprints...your adventures and experiences around the world!</p>
+
+	const msg = {
+	  to: user.email,
+	  from: 'support@footprintlive.org',
+	  subject: 'Welcome to Footprint!',
+	  text: `Welcome to Footprint!
+	  	Now you can start sharing your footprints... your adventures and experiences around the world!
+	  	And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity!
+	  	Footprint needs you to make a difference in the way of travelling and exploring! Let's make it real, let's make it REAL TIME with FOOTPRINT!
+
+	  	Miles of thanks,
+	  	Team Footprint
+	  `,
+	  html: `<h2 style="font-size: 16px;">Welcome to Footprint!</h2>
+      <p>Now you can start sharing your footprints... your adventures and experiences around the world!</p>
       <p>And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! </p>
     	<p>Footprint needs you to make a difference in the way of travelling and exploring! Let's make it real, let's make it REAL TIME with FOOTPRINT! </p>
     	<br/>
     	<div>Miles of thanks,</div>
     	<div>Team Footprint</div>
     `,
-    //text: 'welcome!'
-  };
+	};
+sgMail.send(msg);
+	// const mailOptions = {
+ //    from: 'Footprint Support <footprintlive@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+ //    to: user.email,
+ //    subject: 'Welcome to Footprint!',
+ //    html: `<h2 style="font-size: 16px;">Welcome to Footprint!</h2>
+ //      <p>Now you can start sharing your footprints...your adventures and experiences around the world!</p>
+ //      <p>And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! </p>
+ //    	<p>Footprint needs you to make a difference in the way of travelling and exploring! Let's make it real, let's make it REAL TIME with FOOTPRINT! </p>
+ //    	<br/>
+ //    	<div>Miles of thanks,</div>
+ //    	<div>Team Footprint</div>
+ //    `,
+ //    //text: 'welcome!'
+ //  };
 
   console.log("sending welcom email to user")
 
   //TODO: also send email to kevin and giorgia
 
-  return transporter.sendMail(mailOptions, (erro, info) => {
+//   return transporter.sendMail(mailOptions, (erro, info) => {
+// 
+//     if (erro){
+//       console.error("error sending welcome email: ", erro, ", info: ", info)
+// 
+// 
+//     } else {
+//     	console.log("successfuly sent welcome email! info: ", info)
+//     }
+// 
+//     return null
+//   });
 
-    if (erro){
-      console.error("error sending welcome email: ", error, ", info: ", info)
-
-
-    } else {
-    	console.log("successfuly sent welcome email! info: ", info)
-    }
-
-    return null
-  });
+  return null
 });
 
 
 
 //For testing
-exports.sendMail = functions.https.onRequest((req, res) => {
+exports.inappropriateContentFlag = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
       
         // getting dest email by query string
@@ -108,31 +202,47 @@ exports.sendMail = functions.https.onRequest((req, res) => {
 
   	console.log("sendMail, dest: ", dest)
 
-	  const mailOptions = {
-	    from: 'Footprint Support <footprintlive@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
-	    to: dest,
-	    subject: 'Welcome to Footprint!', // email subject
-	    // html: `<h2 style="font-size: 16px;">Welcome to Footprint!</h2>
-	    //   <p>Now you can start sharing your footprints...your adventures and experiences around the world!</p>
-	    //   <p>And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! </p>
-	    // 	<p>Footprint needs you to make a difference in the way of travelling and exploring! Let's make it real, let's make it REAL TIME with FOOTPRINT! </p>
-	    // 	<br/>
-	    // 	<div>Miles of thanks,</div>
-	    // 	<div>Team Footprint</div>
-	    // `
-	    text: 'welcome test from sendMail!'
-	    // text: 'Now you can start sharing your footprints...your adventures and experiences around the world! And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! Miles of thanks, Team Footprint'
-	  };
-
-	  console.log("sending welcom email to user")
+// 	  const mailOptions = {
+// 	    from: 'Footprint Support <footprintlive@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+// 	    to: dest,
+// 	    subject: 'Welcome to Footprint!', // email subject
+// 	    // html: `<h2 style="font-size: 16px;">Welcome to Footprint!</h2>
+// 	    //   <p>Now you can start sharing your footprints...your adventures and experiences around the world!</p>
+// 	    //   <p>And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! </p>
+// 	    // 	<p>Footprint needs you to make a difference in the way of travelling and exploring! Let's make it real, let's make it REAL TIME with FOOTPRINT! </p>
+// 	    // 	<br/>
+// 	    // 	<div>Miles of thanks,</div>
+// 	    // 	<div>Team Footprint</div>
+// 	    // `
+// 	    text: 'welcome test from sendMail!'
+// 	    // text: 'Now you can start sharing your footprints...your adventures and experiences around the world! And you can follow in real time the footprints of other great people like you and you will never miss a cool opportunity! Miles of thanks, Team Footprint'
+// 	  };
+// 
+// 	  console.log("sending welcom email to user")
 
 	  //TODO: also send email to kevin and giorgia
 
-	  return transporter.sendMail(mailOptions, (erro, info) => {
-	    if(erro) {
-	        return res.send(erro.toString());
-	    }
-	    return res.send('Sended');
-	  });
+	  // return transporter.sendMail(mailOptions, (erro, info) => {
+	  //   if(erro) {
+	  //       return res.send(erro.toString());
+	  //   }
+	  //   return res.send('Sended');
+	  // });
+  	const msg = {
+		  to: dest,
+		  from: 'support@footprintlive.org',
+		  subject: 'Inappropriate content flag received',
+		  text: `Thank you for helping us keep Footprint safe by flagging inappropriate content. We will review the flagged media shortly.
+
+		  	Team Footprint
+		  `,
+		  html: `<h2 style="font-size: 16px;">Inappropriate content flag received</h2>
+	      <p>Thank you for helping us keep Footprint safe by flagging inappropriate content. We will review the flagged media shortly.</p>
+	    	<br/>
+	    	<div>Team Footprint</div>
+	    `,
+		};
+		sgMail.send(msg);
+
   });    
 });
